0ab2d4b96a06571db3060cbef66ff756a939cca0
Aya-Amer <ayaamer694@gmail.com>
2023-03-27 02:31:10 +0200

    sign in component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,29 +6,32 @@ export default function Header() {
     const navigate = useNavigate();
     const location = useLocation();
     function pathMathRoute (route){
+        debugger;
         if (route === location.pathname)
         return true;
-    }
-    console.log(navigate) 
-    
+    }      
   return (
     <div className='bg-white shadow-sm border-b top-0 sticky z-50'>
     <header className='max-w-6xl flex justify-between items-center px-3 mx-auto'>
-      <div><img className="h-5 cursor-pointer" src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo" 
+      <div><img className="h-5 cursor-pointer" src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+       alt="logo" 
       onClick={()=> navigate("/")}/></div>
       <div>
         <ul className='flex space-x-10'>
-            <li to="./" className={`py-3 font-semibold text-gray-400 text-sm
-            cursor-pointer border-b-transparent border-b-[3px] whitespace-nowrap 
-            ${pathMathRoute("/") && "border-b-red-500 text-black"}`} 
+            <li to="./" className={`py-3 font-semibold text-sm
+            cursor-pointer border-b-[3px] whitespace-nowrap 
+            ${pathMathRoute("/") ? "border-b-red-500 text-black":
+            " text-gray-400 border-b-transparent"}`} 
             onClick={()=> navigate("/")} >Home</li>
-            <li to="./offers" className={`py-3  font-semibold text-gray-400 text-sm 
-            cursor-pointer whitespace-nowrap border-b-transparent border-b-[3px] 
-            ${pathMathRoute("/offers") && "border-b-red-500 text-black"}`} 
+            <li to="./offers" className={`py-3 font-semibold  text-sm 
+            cursor-pointer whitespace-nowrap  border-b-[3px] 
+            ${pathMathRoute("/offers") ? "border-b-red-500 text-black"
+            :"text-gray-400 border-b-transparent"}`} 
             onClick={()=> navigate("/offers")}>Offers</li>
-            <li to="./sign-in" className={`py-3  font-semibold text-gray-400 text-sm 
-            whitespace-nowrap cursor-pointer border-b-transparent border-b-[3px] 
-            ${pathMathRoute("/sign-in") && "border-b-red-500 text-black"}`} 
+            <li to="./sign-in" className={`py-3 font-semibold text-gray-400 text-sm 
+            whitespace-nowrap cursor-pointer border-b-[3px] 
+            ${pathMathRoute("/sign-in") ? "border-b-red-500 text-black"
+            :"text-gray-400 border-b-transparent"}`} 
             onClick={()=> navigate("/sign-in")}>Sign in</li>
         </ul>
       </div>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import  { useState } from 'react'
 
 export default function SignIn() {
+    const [formData, setFormData] = useState({
+email:"",
+password:""
+    })
+    const {email,password} = formData ;
+    function changeForm(e){
+    setFormData({...formData,[e.target.id]:e.target.value})
+    }
   return (
-    <div>
-      Sign In
-    </div>
+    <section className='max-w-6xl mx-auto'>
+        <h4 className='text-center font-bold text-3xl mt-6'>Sign In</h4>
+        <div className='flex justify-center items-center h-full g-6 flex-wrap'>
+            <div className='md:w-8/12 lg:w-6/12'>
+            <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            className='rounded-xl'/>
+            </div>
+            <div className='w-full md:w-8/12 lg:w-5/12'>
+                <form>
+                    <input className='w-full pl-3 py-2 font-medium text-xl border rounded bg-white'                     
+                    type="text" id="email" placeholder='Email address' onChange={changeForm}
+                    value={email}/>
+                    <input className='w-full pl-3 py-2 font-medium text-xl border rounded bg-white mt-5' 
+                    id ="password" placeholder='Password' onChange={changeForm}
+                    value={password}/>
+                </form>
+                
+            </div>
+        </div>
+      
+    </section>
   )
 }
