5597ba1e07ac8dbaecca5e4a23cffd8a849e0bca
Aya-Amer <ayaamer694@gmail.com>
2023-04-01 17:43:15 +0200

    functionality for reset password

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,6 +1,8 @@
 import  { useState } from 'react'
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { toast } from 'react-toastify';
 export default function ForgotPassword() {
     const [emailData, setEmailData] = useState({
 email:"",
@@ -9,8 +11,16 @@ email:"",
     function changeEmail(e){
         setEmailData({...emailData,[e.target.id]:e.target.value})
     }
-    function sendResetEmail(e){
+   async function sendResetEmail(e){
         e.preventDefault();
+        try {
+            const auth = getAuth();
+            await sendPasswordResetEmail(auth, email);
+            toast.success("Email was sent")
+        } catch (error) {
+            toast.error("Could not send reset password")
+        }
+        
     }  
   return (
     <section className='max-w-6xl mx-auto px-4'>
@@ -21,7 +31,7 @@ email:"",
             alt="key" className='w-full rounded-2xl'/>
             </div>
             <div className='w-full md:w-8/12 lg:w-5/12 lg:ml-20'>
-                <form>
+                <form onSubmit={sendResetEmail}>
                     <input className='w-full px-4 py-2 text-gray-700 font-medium text-xl border-gray-300 rounded
                      bg-white transition ease-in-out'                     
                     type="text" id="email" placeholder='Email address' onChange={changeEmail}
@@ -32,7 +42,7 @@ email:"",
                            <p><Link to="/sign-in" className='text-blue-600 hover:text-blue-800 transition 
                             duration-200 ease-in-out ml-1'>
                                 Sign in instead?</Link></p> </div>
-                    <button type="submit" onSubmit={sendResetEmail}
+                    <button type="submit" 
                     className='uppercase bg-blue-600 rounded w-full py-3 px-7 text-sm font-medium text-center mt-5 text-white'>
                     send reset email</button>
                     <div className='flex my-4 items-center before:border-t before:flex-1
